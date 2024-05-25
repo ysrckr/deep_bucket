8ed3e82c935c978c15cc0ee2d721022ed1fed24a
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-05-25 23:24:08 +0700

    chore: Update constants.ts to include API base URL configuration

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,1 +1,5 @@
 export const titleBase = 'Deep Bucket | ';
+
+export const apiPostfix = process.env.API_POSTFIX || 'api';
+export const apiVersion = process.env.API_VERSION || 'v1';
+export const apiBaseURL = `/${apiPostfix}/${apiVersion}`;
