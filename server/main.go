68426e881b8ebd97f17f1f8df37ab0b8236519da
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-05-26 02:04:16 +0700

    chore: Update npm dependencies to latest versions

--- server/main.go
+++ server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	v1 := app.Group("/v1")
+
+	v1.Get("/hello/test", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		return c.JSON(fiber.Map{
+			"message": "Hello, World!",
+		})
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":8000"))
+
 }
