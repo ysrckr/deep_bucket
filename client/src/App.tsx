e86de30db7d362473c479c717f46dd645593019e
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-05-25 22:21:51 +0700

    chore: Remove unused CSS styles from App.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,9 @@
 import './App.css';
 
-import { Button } from './components/ui/button';
-import { cn } from './lib/utils';
-
 function App() {
   return (
     <>
-      <div className={cn('bg-red-500', 'p-4')}>
-        <Button
-          variant="default"
-          size="sm"
-          onClick={() => {
-            console.log('Hello World');
-          }}
-        >
-          Hello World
-        </Button>
-      </div>
+      <div></div>
     </>
   );
 }
