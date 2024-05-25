d77fa7961c29f0d6e9506b1b6faf16b316f02cf3
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-05-26 02:04:06 +0700

    chore: Refactor axios configuration and update API base URL handling

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,6 +2,10 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 import { apiBaseURL } from './constants';
 
+export const errorMessages: string[] = [];
+
+console.log(errorMessages);
+
 const request = axios.create({
   baseURL: apiBaseURL,
   withCredentials: true,
@@ -18,6 +22,7 @@ request.interceptors.response.use(
     if (error.response.status === 401) {
       window.location.href = '/login';
     }
+
     return Promise.reject(error);
   },
 );
@@ -31,20 +36,26 @@ request.interceptors.request.use(
   },
 );
 
-export const get = (url: string, config?: AxiosRequestConfig) =>
-  request.get(url, config);
+export const get = (url: string, config?: AxiosRequestConfig) => {
+  return request.get(url, config);
+};
 
 export const post = (
   url: string,
   data?: unknown,
   config?: AxiosRequestConfig,
-) => request.post(url, data, config);
+) => {
+  return request.post(url, data, config);
+};
 
 export const update = (
   url: string,
   data?: unknown,
   config?: AxiosRequestConfig,
-) => request.patch(url, data, config);
+) => {
+  return request.patch(url, data, config);
+};
 
-export const del = (url: string, config?: AxiosRequestConfig) =>
-  request.delete(url, config);
+export const del = (url: string, config?: AxiosRequestConfig) => {
+  return request.delete(url, config);
+};
